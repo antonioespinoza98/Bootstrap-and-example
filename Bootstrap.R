0235e87bb692293559f352f1d2355d6da5414ae2
antonioespinoza98 <espinozamarco70@gmail.com>
2024-07-22 11:17:31 -0400

    New files

---
+++ Bootstrap.R
@@ -0,0 +1,333 @@
+# Bootstrap ---------------------------------------------------------------
+# Autor: Marco Espinoza
+# Comisión Económica para América Latina y el Caribe
+# Julio - 2024
+
+# 1. El sample consiste del error con el replace TRUE para todo el censo para 
+# el error. 
+
+# 2. De ahi con la prediccion y el vector de efectos aleatorios, 
+# estimamos F(X1) + Z1 + E1 para el censo. 
+
+# 3. Media para Dominio para PB e {1,... N} 
+
+# 4. Luego se hace una esperanza para todas las poblaciones Bootstrap
+
+# 5. Se hace lo mismo para la varianza
+
+
+# Del censo vamos a necesitar:
+# 1.1 F(X) que es nuestra funcion estimada
+# 1.2 El dominio
+
+
+# Preparación -------------------------------------------------------------
+library(srvyr)
+library(survey)
+library(data.table)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(XboostingMM)
+
+data <- readRDS("C:/Users/marqu/Documents/SAExML/R/pobreza/datos/encuesta_df_agg.rds") |>
+  mutate_if(is.character, as.factor)
+
+censo <- readRDS("C:/Users/marqu/Documents/SAExML/R/ingreso/datos/cens0.rds") |>
+  select(dam) |>
+  mutate(dam = recode(dam,
+                      "01" = "1",
+                      "02" = "2",
+                      "03" = "3",
+                      "04" = "4",
+                      "05" = "5",
+                      "06" = "6"
+                      ))
+data <- as.data.frame(data)
+# Leemos la predicción
+f <- readRDS("prediction.rds")
+length(f)
+
+# pegamos la predicción al censo
+censo$f <- f
+
+# 2. Efectos aleatorios
+fit <- readRDS("fit.rds")
+randomEffects <- fit$raneffs
+
+# 3. Errores
+errorTerms <- fit$errorTerms
+
+# Calculamos el population B1
+
+dominio <- sort(unique(censo$dam))
+len_dom <- length(censo$dam)
+
+# Tamaño de la base
+ext <- dim(censo)[1]
+
+
+# bias corrected term -----------------------------------------------------
+
+# De acuerdo con Krennmair, Schmid (2022). Como estimador de la varianza
+# residual del modelo, \sigma^2_e esta positivamente sesgado, debido a que
+# incluye incertidumbre de la estimacion de la funcion \hat{f}()
+# Es por esta razon, que los autores proponen una correccion de este sesgo.
+
+# 1. Utlizar las predicciones del modelo final despues de converger
+
+last_pred <- fit$boosting_ensemble$fhat
+
+# 2. Generar B Bootstrap samples
+# 2.1 y*(b) = f(x) + e*(b) donde e*(b) son los valores remuestreados
+# con reemplazo de los residuales marginales centrados 
+y <- data$ingreso
+
+residual_cent <- y - last_pred
+
+# Ahora deberiamos sacar una muestra con reemplazo de los residuales
+# centrados para B bootstrap 
+
+error_samp <- matrix(0, nrow = 681, ncol = 100)
+
+for(i in 1:100){
+  error_samp[, i] <- sample(residual_cent, size = 681, replace = TRUE)
+}
+
+# Hacemos una estimacion de y_b
+
+y_B <- matrix(0, nrow = 681, ncol = 100)
+
+for(i in 1:100) {
+  y_B[, i] <- last_pred + error_samp[, i]
+}
+
+colnames(y_B) <- paste0("y_b", 1:100)
+
+sqrterror <- function(preds, dtrain){
+  labels <- xgboost::getinfo(dtrain, "label")
+  grad <- preds - labels
+  hess <- rep(1, length(grad))
+  return(list(grad = grad, hess = hess))
+}
+
+formula <- y_b ~ area + sexo + edad + F182013_stable_lights  + X2016_crops.coverfraction + X2016_urban.coverfraction + X2016_gHM + accessibility + accessibility_walking_only + area1 + sexo2 + edad2 + edad3 + edad4 + edad5 + anoest2 + anoest3 + anoest4 + discapacidad1 + etnia1 + tiene_alcantarillado + tiene_electricidad + tiene_acueducto + tiene_gas + eliminar_basura + tiene_internet + piso_tierra + material_paredes + material_techo + rezago_escolar + alfabeta + hacinamiento + tasa_desocupacion
+
+f_boot <- matrix(0, nrow = 681, ncol = 100)
+
+for(i in 1:100) {
+  # Primero debemos pegarle la variable a la base de datos,
+  # tomando en cuenta que la base debe reiniciarse despues de cada
+  # iteracion para evitar un traslape.
+  temp_data <- data
+  temp_data$y_b <- y_B[, i]
+  
+  
+  # Ahora debemos: estimar la funcion para cada b bootstrap, con y_b(b)
+  #  como variable dependiente.
+  
+  modelo <- xboosting(
+    formula = formula,
+    data = temp_data,
+    loss = sqrterror,
+    n.trees = 1,
+    shrinkage = 0.6,
+    interaction.depth = 9,
+    minsplit = 1,
+    lambda = 1,
+    alpha = 4,
+    subsample = 0.5
+  )
+  
+  # extraemos la prediccion 
+  
+  f_boot[,i] <- modelo$fhat
+  
+}
+
+# Estimamos el termino de correccion k(f)
+k <- 1/100 * sum( (last_pred - f_boot)^2   )
+
+saveRDS(k, "fact_correction.rds")
+
+
+# bias-corrected estimator for the residual variance ----------------------
+
+k <- readRDS("fact_correction.rds")
+
+sigma_corrected <- fit$errorVar - k
+
+
+# Bootstrap de la incertidumbre -------------------------------------------
+
+# A lo planteado por Krennmair, Schmid (2022)
+
+# 1. Se calcula el vector de residuales marginales como 
+# \hat{e}_i = y_i - \hat{f}(X_i)
+
+f <- readRDS("prediction_on_training.rds")
+
+resid_marg <- y - f
+
+data$resid_marg <- resid_marg
+
+
+# 2. residuales nivel-2
+
+r_bar_by_dam <- data |>
+  group_by(dam) |>
+  summarise(
+   r_bar = 1/681 * sum(resid_marg) 
+  )
+
+r_bar <- r_bar_by_dam$r_bar
+
+# 3. Residuales marginales nivel-1
+
+r_hat <- resid_marg - 1*r_bar
+
+# Centrar y escalar por los factores de corrección
+
+
+
+# Ciclo -------------------------------------------------------------------
+count <- 1
+limit <- 100
+ext <- dim(censo)[1]
+PBS <- matrix(0, nrow = ext, ncol = limit)
+result <-  vector(mode = "numeric", length = ext)
+
+while (count <= limit) {
+  # sample del error
+  errorPB <- sample(errorTerms, ext, replace = TRUE)
+  
+  for (dom in dominio) {
+    # Primero verificamos que en el ciclo identificamos el dominio en el censo
+    # Del cual deberia devolver las 1437686 observaciones con los dominios
+    # identificados.
+    
+    # Hay que obtener el vector F filtrado para cada dominio
+    indices <- which(censo$dam == dom)
+    
+    random_effect <- randomEffects[as.numeric(dom)]
+    
+    result[indices] <- censo$f[indices] + random_effect + errorPB[indices]
+  }
+  
+  PBS[, count] <- result
+  count <- count + 1
+  
+}
+
+colnames(PBS) <- paste0("PB", 1:limit)
+
+PBS <- as.data.frame(PBS)
+
+PBS$dam <- censo$dam
+
+
+# Cálculo de medias para cada dominio -------------------------------------
+PBS_long <- PBS |>
+  pivot_longer(cols = starts_with("PB"),
+               names_to = "PB",
+               values_to = "value")
+
+mean_df <- PBS_long |>
+  group_by(dam, PB) |>
+  summarise(media = mean(value, na.rm = TRUE)) |>
+  pivot_wider(names_from = PB, values_from = media)
+
+# Cálculo de medias para todas las PB -------------------------------------
+
+medias <- numeric(6)
+varianzas <- numeric(6)
+
+for (i in 1:6) {
+  medias[i] <- mean(as.numeric(mean_df[i, -1]), na.rm = TRUE)
+  varianzas[i] <- var(as.numeric(mean_df[i, -1]), na.rm = TRUE)
+}
+
+resultado <- matrix(c(medias, varianzas), nrow = 6, ncol = 2,
+                    dimnames = list(
+                      "Dam" = c("01", "02",
+                                "03", "04",
+                                "05", "06"),
+                      "Estimación" = c("ingreso","Varianza")
+                    ))
+
+resultado <- as_tibble(resultado)
+resultado$dam <- c("01","02","03","04","05","06")
+
+data <- readRDS("val_set.rds") |>
+  mutate_if(is.character, as.factor) |>
+  select(dam, fep, dam)
+
+diseno <- data  %>% 
+  as_survey_design(weights = fep)
+
+media_est_srvyr <- svyby(
+  ~ingreso,
+  by = ~dam,
+  design = diseno,
+  svymean
+)
+
+media_est_srvyr$dam <- c("1","2","3","4","5","6")
+
+
+mse <- numeric(6)
+
+for (i in 1:6) {
+  dam_value <- media_est_srvyr$dam[i]
+  observed_mean <- media_est_srvyr$ingreso[i]
+  
+  bootstrap_means <- as.numeric(mean_df[[which(mean_df$dam == dam_value)]])
+  mse[i] <- mean((bootstrap_means - observed_mean)^2, na.rm = TRUE)
+}
+
+mse_result <- tibble(
+  dam = media_est_srvyr$dam,
+  MSE = mse
+)
+
+
+# Validación --------------------------------------------------------------
+
+diseno <- PBS_long  %>% 
+  as_survey_design()
+
+media_est_srvyr <- svyby(
+  ~ingreso,
+  by = ~dam,
+  design = diseno,
+  svymean
+)
+
+confint_int <- confint(svyby(formula = ~ingreso,
+                             by = ~dam,
+                             design = diseno,
+                             svymean))
+
+confint_int <- data.table(confint_int, keep.rownames = TRUE)
+colnames(confint_int) <- c("dam","min_inter", "max_inter")
+
+confint_int$ingreso <- media_est_srvyr$ingreso
+
+mapping <- rbind(media_est_srvyr[,-3], resultado)
+
+estim <- c(rep("Directo", 6), rep("XGBoost MM", 6))
+
+mapping <- cbind(mapping, estim)
+options(scipen = 999)
+mapping |>
+  ggplot(aes(x = dam, y = ingreso)) + geom_point(aes(color = estim), size = 2, position = "jitter") +
+  scale_color_manual(values = c("red", "green")) +
+  ylim(150000,450000) +
+  labs(x = "Región de planificación", y = "Ingreso", col = "Modelo") +
+  geom_errorbar(data = confint_int, aes(x = dam, ymin = min_inter, ymax = max_inter)) +
+  theme_minimal()
+
+
+
+
+
---
+++ MEM_Boosting_IllustrativeExample.R
@@ -0,0 +1,250 @@
+################################################################################
+# Illustrative Data Example                                                 
+################################################################################
+
+#==============================================================================
+# 0. Preparation
+#==============================================================================
+
+#- load packages and import data:
+library(lme4)
+library(stan4bart)
+library(dplyr)
+library(XboostingMM)
+
+set.seed(1998)
+
+data <- readRDS("encuesta_df_agg.rds") |>
+  mutate_if(is.character, as.factor)
+
+censo <- readRDS("cens0.rds") |>
+  mutate(
+  region = recode(dam,
+                  "01" = "Central",
+                  "02" = "Chorotega",
+                  "03" = "Pacífico Central",
+                  "04" = "Brunca",
+                  "05" = "Huetar Caribe",
+                  "06" = "Huetar Norte")) %>%
+  mutate(region = as.factor(region))
+
+
+data <- as.data.frame(data)
+censo <- as.data.frame(censo)
+
+dfsTrain <- data
+dfsTest1 <- censo
+
+#- set parameters 
+conv_memboost = 0.001; maxIter_memboost = 1000; verbose_memboost = F;minIter_memboost = 0
+
+#- specify model
+formula <- ingreso ~ area + sexo + edad + F182013_stable_lights  + X2016_crops.coverfraction + X2016_urban.coverfraction + X2016_gHM + accessibility + accessibility_walking_only + area1 + sexo2 + edad2 + edad3 + edad4 + edad5 + anoest2 + anoest3 + anoest4 + discapacidad1 + etnia1 + tiene_alcantarillado + tiene_electricidad + tiene_acueducto + tiene_gas + eliminar_basura + tiene_internet + piso_tierra + material_paredes + material_techo + rezago_escolar + alfabeta + hacinamiento + tasa_desocupacion
+random <- ~ 1|dam
+
+modelLMM <- ingreso ~ (1|dam) + area + sexo + edad + F182013_stable_lights  + X2016_crops.coverfraction + X2016_urban.coverfraction + X2016_gHM + accessibility + accessibility_walking_only + area1 + sexo2 + edad2 + edad3 + edad4 + edad5 + anoest2 + anoest3 + anoest4 + discapacidad1 + etnia1 + tiene_alcantarillado + tiene_electricidad + tiene_acueducto + tiene_gas + eliminar_basura + tiene_internet + piso_tierra + material_paredes + material_techo + rezago_escolar + alfabeta + hacinamiento + tasa_desocupacion
+
+#- estimate the ICC in training data
+fitICCLMM <- lme4::lmer(ingreso ~ (1 | dam), data = dfsTrain, REML = FALSE)
+summary(fitICCLMM)
+dfV <- lme4::VarCorr(fitICCLMM)
+ICC <- dfV$dam[1] / ( dfV$dam[1] + sigma(fitICCLMM)^2 )
+ICC 
+#==============================================================================
+# 1. Model estimation
+#==============================================================================  
+
+#-----------------------------------
+#- some preparations
+#-----------------------------------
+result <- NULL
+PredNames <- attr( stats::terms( formula ), "term.labels" )
+OutcomeName <-  all.vars(formula)[1]
+XTrain <- as.data.frame(dfsTrain[, PredNames, drop = FALSE]) 
+XTest1 <- as.data.frame(dfsTest1[, PredNames, drop = FALSE])
+YTrain <- as.matrix(dfsTrain[, OutcomeName, drop = FALSE]) 
+
+
+#-------------------------------------------------------------------------------
+#- MEM Boosting 
+#-------------------------------------------------------------------------------
+s_BoostMERT_L2 <- Sys.time()
+# ================================================================
+# CUSTOM OBJECTIVE FUNCTION
+# ================================================================
+
+sqrterror <- function(preds, dtrain){
+  labels <- xgboost::getinfo(dtrain, "label")
+  grad <- preds - labels
+  hess <- rep(1, length(grad))
+  return(list(grad = grad, hess = hess))
+}
+
+fitBoostMERT_L2 <- boost_mem(
+  formula,
+  data = dfsTrain,
+  random = random,
+  shrinkage = 0.6,
+  interaction.depth = 9,
+  n.trees = 100,
+  loss = sqrterror,
+  minsplit = 1,
+  subsample = 0.5,
+  lambda = 1,
+  alpha = 4,
+  verbose_memboost = verbose_memboost,
+  minIter_memboost = minIter_memboost,
+  maxIter_memboost = maxIter_memboost
+)
+
+e_BoostMERT_L2 <- Sys.time()
+time_BoostMERT_L2 <- difftime(e_BoostMERT_L2, s_BoostMERT_L2, units = "secs")[[1]]
+
+#- Calculate predicted values
+# tmpdfsTest1 <- merge(dfsTest1, data.frame(dam = unique(dfsTrain$dam), raneffs = fitBoostMERT_L2$raneffs))
+
+fhat_Test1 <- XboostingMM:::predict.xgb(fitBoostMERT_L2$boosting_ensemble,
+                          newdata = dfsTest1,
+                          n.trees = 100)
+
+saveRDS(fhat_Test1, file = "prediction.rds")
+saveRDS(fitBoostMERT_L2, file = "fit.rds")
+
+
+
+# perform prediction on training set --------------------------------------
+
+pred_training <- XboostingMM:::predict.xgb(fitBoostMERT_L2$boosting_ensemble,
+                          newdata = dfsTrain,
+                          n.trees = 100)
+
+
+saveRDS(pred_training, "prediction_on_training.rds")
+
+Yhat_Test1 <-  readRDS("C:/Users/marqu/OneDrive/Documents/osfstorage-archive - Copy/Functions for (MEM) gradient tree boosting/prediction.rds")
+
+
+val = validation(test = dfsTest1, prediction = prediction, validation_set = val_set,
+           weights = "fep", label = "ingreso", region = "dam", model = 
+           )
+#-------------------------------------------------------------------------------
+#- Bayesian Additive Regression Tree with random intercept
+#-------------------------------------------------------------------------------
+s_BART <- Sys.time()
+fitBART <- stan4bart::stan4bart(
+  formula = ingreso ~ (1|dam) + bart(F182013_stable_lights + X2016_crops.coverfraction + X2016_urban.coverfraction + X2016_gHM + accessibility + accessibility_walking_only + area1 + sexo2 + edad2 + edad3 + edad4 + edad5 + anoest2 + anoest3 + anoest4 + discapacidad1 + etnia1 + tiene_alcantarillado + tiene_electricidad + tiene_acueducto + tiene_gas + eliminar_basura + tiene_internet + piso_tierra + material_paredes + material_techo + rezago_escolar + alfabeta + hacinamiento + tasa_desocupacion),
+  verbose = -1, # suppress ALL output
+  data = dfsTrain,
+  bart_args = list(keepTrees = T) ) # needed for prediction of (new) test data
+
+e_BART <- Sys.time()
+time_BART <- difftime(e_BART, s_BART, units = "secs")[[1]]
+
+#- Calculate predicted values
+Yhat_BART_Test1 <- rowMeans( predict(fitBART, dfsTest1, type = "ev") )
+Yhat_BART_Test2 <- rowMeans( predict(fitBART, dfsTest2, type = "ev") )
+
+
+#-------------------------------------------------------------------------------
+#- MERF
+#-------------------------------------------------------------------------------
+
+library(SAEforest)
+
+ingreso <- dfsTrain$ingreso
+X_covar <- dfsTrain[,PredNames]
+
+model1 <- SAEforest_model(Y = ingreso,
+                          X = X_covar,
+                          dName = "dam",
+                          smp_data = dfsTrain,
+                          pop_data = dfsTest1)
+
+summary(model1)
+#-------------------------------------------------------------------------------
+#- Standard Random Forest 
+#-------------------------------------------------------------------------------
+s_RF <- Sys.time()
+fitRF <- randomForest::randomForest(formula = model, data = dfsTrain)
+e_RF <- Sys.time()
+time_RF <- difftime(e_RF, s_RF, units = "secs")[[1]]
+
+#- Calculate predicted values
+Yhat_RF_Test1 <- predict(fitRF, dfsTest1)
+
+
+#-------------------------------------------------------------------------------
+#- Linear Mixed Model
+#-------------------------------------------------------------------------------
+s_LMM <- Sys.time()
+fitLMM <- lme4::lmer(modelLMM, weights = n, data = dfsTrain)
+e_LMM <- Sys.time()
+time_LMM <- difftime(e_LMM, s_LMM, units = "secs")[[1]]
+
+Yhat_LMM_Test1 <- predict( fitLMM, dfsTest1 )
+
+
+#-------------------------------------------------------------------------------
+#- Calculate PMSE and R2 for both test sets
+#-------------------------------------------------------------------------------
+Yhat_list_Test1 <- list( BoostMERT_L2 = Yhat_MEMBoost_Test1[, grepl("BoostMERT_L2", colnames(Yhat_MEMBoost_Test1))],
+                         LMM = Yhat_LMM_Test1 )
+
+nEstimators <- length(Yhat_list_Test1)
+EstimatorNames <- names(Yhat_list_Test1)
+resList <- sapply(EstimatorNames, function(x) NULL) 
+
+for (x in 1:nEstimators){
+  PMSE_Test1 <- mean( (YTest1 - Yhat_list_Test1[[x]])^2 )
+  R2_Test1 <- cor(YTest1, Yhat_list_Test1[[x]])^2
+  resList[[x]] <- rbind( resList[[x]],
+                         c(  Estimator = x,
+                             PMSE_Test1 = PMSE_Test1,
+                             R2_Test1 = R2_Test1
+                             ) )
+}
+resPMSE_R2 <- do.call("rbind", resList) 
+resPMSE_R2 <- data.frame(resPMSE_R2)
+resPMSE_R2$Estimator <- EstimatorNames 
+cbind(resPMSE_R2$Estimator, round(resPMSE_R2[, 2:3], 2))
+
+# computation times of the different approaches
+resCompTimes <- data.frame( Estimator = EstimatorNames,
+                            ComputationTime = c(time_BoostMERT_L2, time_BoostREEM_L2,
+                              time_LMM ) )
+cbind(resCompTimes$Estimator, round(resCompTimes$ComputationTime, 2) )
+
+
+
+#==============================================================================
+#- 2. Get estimated variance of random intercepts and estimated error variance
+#==============================================================================
+Dhat <- matrix(
+  c(
+    fitBoostMERT_L2$var_random_effects,
+    fitted(fitBART,  type = "Sigma")$dam,
+    lme4::VarCorr(fitLMM)$dam[1]
+  ),
+  nrow = 3,
+  dimnames = list(Model = c("BoostMERT_L2","BART", "LMM"), "Variance of random intercepts")
+  
+)
+
+# Varibility betweeen planification regions (between-group variance)
+Dhat
+
+
+ErrorVar <- matrix(
+  c(fitBoostMERT_L2$errorVar,
+    47468214004,
+    fitted(fitBART,  type = "sigma")^2,
+    sigma(fitICCLMM) ^ 2),
+  nrow = 4,
+  dimnames = list(Model = c("BoostMERT_L2","MERF","BART", "LMM"), "Variance of residual errors")
+)
+# Variance of the residual errors (not explained by either fixed effects or random effects)
+ErrorVar
+
+
+
+
